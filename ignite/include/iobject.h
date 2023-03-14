ef01b09152d718e51ba9112b8581c9fcf4c6da9e
Yegender Kumar <yegenderkumar124001@gmail.com>
2023-03-14 16:48:50 +0530

    didn't solved last mistake

---
+++ ignite/include/iobject.h
@@ -0,0 +1,6 @@
+/**
+* @file iobject.h
+ * @author Yegender Kumar
+ * @date March 2023
+ * @brief Ignite Object class
+*/
---
+++ ignite/include/itype.h
@@ -0,0 +1,6 @@
+/**
+* @file itype.h
+ * @author Yegender Kumar
+ * @date March 2023
+ * @brief Custom type defines used by the OS Parts and other apis such as Radium etc.
+*/
